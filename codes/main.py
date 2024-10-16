494b51fe92b2d60ca60b385d0c793de5918f3543
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-16 18:58:16 +0500

    my thesis

--- codes/main.py
+++ codes/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 # Load a color image in grayscale
-img = cv2.imread('pictures/OpenCV_Logo.png',1)
+img = cv2.imread('/pictures/rasm1.png',1)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
