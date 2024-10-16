64cd5e0a9792e501f32f52301e301065b00699cf
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-16 19:05:23 +0500

    my thesis

--- codes/main.py
+++ codes/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-# Load a color image in grayscale
-img = cv2.imread('/pictures/rasm1.png',1)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import matplotlib.pyplot as plt
+# Load an color image in grayscale
+img = cv2.imread('rasm1.png',0)
+plt.imshow(img)
+plt.show()
