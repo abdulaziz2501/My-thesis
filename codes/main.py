fd80c8eb82131b3302f3e6afa884cb5c6bb7e6b2
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-16 19:12:31 +0500

    my thesis

--- codes/main.py
+++ codes/main.py
@@ -2,6 +2,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 # Load an color image in grayscale
-img = cv2.imread('rasm1.png',0)
+img = cv2.imread('rasm1.png',)
 plt.imshow(img)
 plt.show()
