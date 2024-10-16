053f8f5563dbb6451e5352f687ee6b749bacf60d
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-16 20:51:12 +0500

    my thesis

--- codes/rasm_rangini _aniqlash.py
+++ codes/rasm_rangini _aniqlash.py
@@ -22,3 +22,14 @@ import numpy as np
 # cv2.imshow('NOT B', cv2.bitwise_not(img2))
 # if cv2.waitKey(0) & 0xff == 27:
 #     cv2.destroyAllWindows()
+
+import numpy as np
+import cv2
+img = cv2.imread('rasm2.png',1)
+cv2.line(img,(20,400),(400,20),(255,255,255),3)
+cv2.rectangle(img,(200,100),(400,400),(0,255,0),5)
+cv2.circle(img,(80,80), 55, (255,255,0), -1)
+cv2.ellipse(img, (300,425), (80, 20), 5, 0, 360, (0,0,255), -1)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
